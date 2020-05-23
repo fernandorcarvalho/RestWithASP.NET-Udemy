41bf536660c56dc32ef9ce76e3a1b657590cf779
Fernando Carvalho <fernandorcarvalho@gmail.com>
2020-05-23 13:40:22 -0300

    Implementing all principal verbs

---
+++ RestWithASP.NETUdemy - 02 -UsingDiferentVerbs/RestWithASP.NETUdemy/Controllers/Model/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASP.NETUdemy.Controllers.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string SecondName { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+    }
+}
